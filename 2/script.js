edf3c2d0deacfb6ba4b7a5a495fd46036a292616
Gopi <208a1a0543@gmail.com>
2026-01-05 19:59:19 +0530

    Adding

---
+++ 2/script.js
@@ -0,0 +1,14 @@
+function changeTextColor(color){
+    let h2 = document.getElementById('head')
+    h2.style.color=color
+}
+
+function changeBackgroundColor(color){
+    let h2 = document.getElementById('head')
+    h2.style.backgroundColor=color
+}
+
+function changeFont(font){
+     let h2 = document.getElementById('head')
+    h2.style.fontFamily=font
+}
